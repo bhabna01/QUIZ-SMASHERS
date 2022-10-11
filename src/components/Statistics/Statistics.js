cb9c4e9e2cf130069c8935b89378587d4fcc9367
bhabna01 <abierhoque01@gmail.com>
2022-10-11 21:54:07 +0600

    5th commit

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { AreaChart, Area, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const topics=useLoaderData();
+    const {data}=topics;
+    console.log(data);
+    
+   
+    
     return (
-        <div>
-            
+        <div style={{ width: '100%', height: 300 }}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
         </div>
+           
+        
     );
 };
 
