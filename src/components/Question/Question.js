e19ad825aee2e01338aa26868aef2ed40b72a48f
bhabna01 <abierhoque01@gmail.com>
2022-10-11 23:48:01 +0600

    9th commit

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -2,18 +2,23 @@ import React from "react";
 import Option from "../Option/Option";
 import './Question.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee  ,faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 const Question = ({ question }) => {
     const { options } = question;
     console.log(question);
+    console.log(question.length);
   const lastDigit = question.id.slice(question.id.length - 1);
   const quizNo = parseInt(lastDigit);
   console.log(quizNo);
+//   const ques= question.question.slice (3, -4)
+  
+  const ques=question.question.replace( /(<([^>]+)>)/ig, '');
+  
   return (
-    <div className="container question w-50">
+    <div className="container question w-50" >
       
-          <div className="d-flex question-info">
-          <h4> Quiz {quizNo + 1}: {question.question}</h4>  <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+          <div className="d-flex question-info" >
+          <h4> {ques}</h4>  <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
             </div>
             
               <div className="option-container">
