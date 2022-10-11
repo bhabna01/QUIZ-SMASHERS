ec8a1b9ad2a0e12c38e03eed9e3e9ff2460756ec
bhabna01 <abierhoque01@gmail.com>
2022-10-12 01:59:49 +0600

    10th commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,17 +5,28 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <nav className='header'>
-            <div>
-                <h2>QuizSmashers</h2>
-            </div>
-             <div>
-                <Link to="/">Home</Link>
-                {/* <Link to={`/quiz/${id}`}>Quizes</Link> */}
-                <Link to="/statistics">Statistics</Link>
-                <Link to="/blog">Blog</Link>
-            </div>
-        </nav>
+        <nav class="navbar navbar-expand-lg header">
+  <div class="container-fluid">
+  <h2>QuizSmashers</h2>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <Link to="/">Home</Link>
+        </li>
+        <li class="nav-item">
+        <Link to="/statistics">Statistics</Link>
+        </li>
+        <li class="nav-item">
+        <Link to="/blog">Blog</Link>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
     );
 };
 
